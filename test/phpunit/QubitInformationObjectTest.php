e618193acda5a72e981e02765daf1693689d82f2
Steve Breker <sbreker@artefactual.com>
2025-02-25 12:59:43 -0800

    Ensure uniqueness in test data

--- test/phpunit/QubitInformationObjectTest.php
+++ test/phpunit/QubitInformationObjectTest.php
@@ -18,47 +18,50 @@ class QubitInformationObjectTest extends TestCase
      * @param string      $identifier    the information object identifier
      * @param string      $title         the information object title
      * @param null|string $repoName      the repository authorized_form_of_name
+     * @param mixed       $hasLinkedRepo boolean indicating if the information object should have a linked repository
      * @param null|string $expectedTitle expected localized title if a match is found; otherwise, null
-     * @param mixed       $hasLinkedRepo
      */
     public function testGetByTitleIdentifierAndRepo($identifier, $title, $repoName, $hasLinkedRepo, $expectedTitle)
     {
-        if (null !== $this->io) {
-            $io = $this->io;
-        } else {
-            $io = new QubitInformationObject();
-        }
+        $randomString = rand(1000000, 9999999);
 
-        $io->title = 'TestDescriptionTitle';
-        $io->identifier = 'TestDescriptionIdentifier';
+        $io = new QubitInformationObject();
+        $io->title = 'TestDescriptionTitle'.$randomString;
+        $io->identifier = 'TestDescriptionIdentifier'.$randomString;
 
+        // Set up a linked repository if needed for the test.
         if (true === $hasLinkedRepo) {
-            if (null !== $this->repo) {
-                $repository = $this->repo;
-            } else {
-                $repository = new QubitRepository();
-            }
             $repository = new QubitRepository();
             $repository->indexOnSave = false;
-            $repository->setAuthorizedFormOfName('TestRepository');
+            $repository->setAuthorizedFormOfName('TestRepository'.$randomString);
             $repository->save();
             $io->setRepositoryId($repository->id);
-            $this->repo = $repository;
         }
 
         $io->indexOnSave = false;
         $io->save();
-        $this->io = $io;
 
-        $result = QubitInformationObject::getByTitleIdentifierAndRepo($identifier, $title, $repoName);
+        if (null !== $repoName) {
+            $repoName .= $randomString;
+        }
+
+        $result = QubitInformationObject::getByTitleIdentifierAndRepo(
+            $identifier.$randomString,
+            $title.$randomString,
+            $repoName
+        );
 
         if (null === $expectedTitle) {
             // No match expected.
-            $this->assertNull($result, 'Expected null when no matching record exists.');
+            $this->assertNull($result, 'Expected null result when no matching record exists.');
         } else {
             // A match is expected.
             $this->assertNotNull($result, 'Expected a valid integer id when data should match.');
             $this->assertIsInt($result, 'Expected the returned id to be an integer.');
+
+            $resultIo = QubitInformationObject::getById($result);
+            $this->assertNotNull($resultIo, 'Expected a valid information object.');
+            $this->assertEquals($expectedTitle.$randomString, $resultIo->title, 'The information object title does not match expected.');
         }
     }
 
@@ -69,15 +72,15 @@ class QubitInformationObjectTest extends TestCase
             // Id, title and repository specified but repo not linked: matching fail.
             ['TestDescriptionIdentifier', 'TestDescriptionTitle', 'TestRepository', false, null],
             // Id, title specified only and repo is linked: matching success.
-            ['TestDescriptionIdentifier', 'TestDescriptionTitle', null, false, 'exampleTitle1'],
+            ['TestDescriptionIdentifier', 'TestDescriptionTitle', null, false, 'TestDescriptionTitle'],
             // Id, title and repository specified but title not matched and repo missing: matching fail.
             ['TestDescriptionIdentifier', 'TestDescriptionTitleX', 'TestRepository', false, null],
             // Id, title and repository specified but id not matched and repo missing: matching fail.
             ['TestDescriptionIdentifierX', 'TestDescriptionTitle', 'TestRepository', false, null],
             // Id, title and repository specified in lookup & all exist: matched.
-            ['TestDescriptionIdentifier', 'TestDescriptionTitle', 'TestRepository', true, 'exampleTitle1'],
+            ['TestDescriptionIdentifier', 'TestDescriptionTitle', 'TestRepository', true, 'TestDescriptionTitle'],
             // Id, title specified only & repo not linked: matching success.
-            ['TestDescriptionIdentifier', 'TestDescriptionTitle', null, true, 'exampleTitle1'],
+            ['TestDescriptionIdentifier', 'TestDescriptionTitle', null, true, 'TestDescriptionTitle'],
             // Id, title and repository specified but repo not matched: matching fail.
             ['TestDescriptionIdentifier', 'TestDescriptionTitle', 'TestRepositoryX', true, null],
             // Id, title and repository specified but title not matched: matching fail.
