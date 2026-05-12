a2f56a5bd51ec2075c09c363ba9e87a88a931179
Julien Ganivet <jganivet1006@gmail.com>
2026-05-12 10:27:29 +0200

    Remove processinfo from public EAD exports

--- plugins/sfEadPlugin/modules/sfEadPlugin/templates/indexSuccessBody.xml.php
+++ plugins/sfEadPlugin/modules/sfEadPlugin/templates/indexSuccessBody.xml.php
@@ -262,24 +262,6 @@
     <custodhist encodinganalog="<?php echo $ead->getMetadataParameter('custodhist'); ?>"><p><?php echo escape_dc(esc_specialchars($value)); ?></p></custodhist>
   <?php } ?>
 
-  <?php $archivistsNotes = $resource->getNotesByType(['noteTypeId' => QubitTerm::ARCHIVIST_NOTE_ID]); ?>
-  <?php 'isad' == $template ? $datesOfCreation = 'app_element_visibility_isad_control_dates' : $datesOfCreation = 'app_element_visibility_rad_control_dates'; ?>
-  <?php $revisionHistoryVisible = 0 < strlen($value = $resource->getRevisionHistory(['cultureFallback' => true])) && ($authenticated || (1 == sfConfig::get($datesOfCreation) && !$findingAid)); ?>
-  <?php $archivistsNotesVisible = 0 < count($archivistsNotes) && ($authenticated || 1 == sfConfig::get('app_element_visibility_isad_control_archivists_notes')); ?>
-  <?php if ($revisionHistoryVisible || $archivistsNotesVisible) { ?>
-    <processinfo>
-      <?php if ($revisionHistoryVisible) { ?>
-        <p><date><?php echo escape_dc(esc_specialchars($value)); ?></date></p>
-      <?php } ?>
-
-      <?php if ($archivistsNotesVisible) { ?>
-        <?php foreach ($archivistsNotes as $note) { ?>
-          <p><?php echo escape_dc(esc_specialchars($note->getContent(['cultureFallback' => true]))); ?></p>
-        <?php } ?>
-      <?php } ?>
-    </processinfo>
-  <?php } ?>
-
   <?php if (0 < strlen($value = $resource->getLocationOfOriginals(['cultureFallback' => true]) ?? '')) { ?>
     <originalsloc encodinganalog="<?php echo $ead->getMetadataParameter('originalsloc'); ?>"><p><?php echo escape_dc(esc_specialchars($value)); ?></p></originalsloc>
   <?php } ?>
@@ -414,16 +396,6 @@
 
       <?php if (0 < strlen($value = $descendant->getArchivalHistory(['cultureFallback' => true])) && ($authenticated || (1 == sfConfig::get('app_element_visibility_rad_archival_history') && !$findingAid))) { ?>
         <custodhist encodinganalog="<?php echo $ead->getMetadataParameter('custodhist'); ?>"><p><?php echo escape_dc(esc_specialchars($value)); ?></p></custodhist>
-      <?php } ?>
-
-      <?php if (0 < strlen($value = $descendant->getRevisionHistory(['cultureFallback' => true]) ?? '') && ($authenticated || (1 == sfConfig::get($datesOfCreation) && !$findingAid))) { ?>
-        <processinfo><p><date><?php echo escape_dc(esc_specialchars($value)); ?></date></p></processinfo>
-      <?php } ?>
-
-      <?php if (0 < count($archivistsNotes = $descendant->getNotesByType(['noteTypeId' => QubitTerm::ARCHIVIST_NOTE_ID])) && ($authenticated || 1 == sfConfig::get('app_element_visibility_isad_control_archivists_notes'))) { ?>
-        <?php foreach ($archivistsNotes as $note) { ?>
-          <processinfo><p><?php echo escape_dc(esc_specialchars($note->getContent(['cultureFallback' => true]))); ?></p></processinfo>
-        <?php } ?>
       <?php } ?>
 
       <?php if (0 < strlen($value = $descendant->getLocationOfOriginals(['cultureFallback' => true]) ?? '')) { ?>
